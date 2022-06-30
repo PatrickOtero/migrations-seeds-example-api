cc5c37db15ef5dc929ffb21c6119c065ac27bad4
Vanessa  Carvalho <80365265+devanessacarvalho@users.noreply.github.com>
2022-06-30 05:44:09 -0300

    feat: populando loja de teste

---
+++ src/seeds/20220630082042_usuario.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  const lojaTeste = {
+    nome: 'Vanessa Carvalho',
+    nome_loja: 'The World',
+    email: 'vanessa@email.teste.com',
+    senha: 'senhadificil'
+  }
+
+  const table = "usuarios"
+
+  const usuarioInserido = await knex(table)
+    .where({ email: lojaTeste.email })
+    .first();
+
+  !usuarioInserido && await knex(table).insert(lojaTeste);
+};
