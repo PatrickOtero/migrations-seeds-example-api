3cc8291348e615ebf83ff4f75e1df69ed518414f
Vanessa  Carvalho <80365265+devanessacarvalho@users.noreply.github.com>
2022-06-30 06:04:13 -0300

    style: ajustes de formatação na seed de usuario de teste

--- src/seeds/20220630082042_usuario.js
+++ src/seeds/20220630082042_usuario.js
@@ -1,16 +1,12 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
 exports.seed = async function(knex) {
   const lojaTeste = {
     nome: 'Vanessa Carvalho',
     nome_loja: 'The World',
     email: 'vanessa@email.teste.com',
     senha: 'senhadificil'
-  }
+  };
 
-  const table = "usuarios"
+  const table = 'usuarios';
 
   const usuarioInserido = await knex(table)
     .where({ email: lojaTeste.email })
